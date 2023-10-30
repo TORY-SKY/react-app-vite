d127f4a6bd95abc78fc257521b279dccd6668e92
TORY-SKY <ezevictor559@gmail.com>
2023-10-30 16:32:37 +0100

    ReactJs

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 function ListGroup() {
   const Cities = ["Lagos", "Ibadan", "Ilorin", "Kano"];
   return (
-    <Fragment>
+    <>
       <h2>A bunch of List Items</h2>
       <ul className="list-group">
         {Cities.map((city) => (
@@ -11,7 +11,7 @@ function ListGroup() {
           </li>
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 export default ListGroup;
