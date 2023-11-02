ec78698c05be171e6a23b145a44276b3a5ff1e9b
TORY-SKY <ezevictor559@gmail.com>
2023-11-02 13:06:01 +0100

    THE_useState_Hook _in_react

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,37 @@
-import { Fragment } from "react";
-//import { MouseEvent } from "react";
+// Import the necessary React components and hooks.
+import React, { useState } from "react";
+
 function ListGroup() {
+  // Define an array of cities.
   let Cities = ["Lagos", "Ibadan", "Ilorin", "Port Harcourt"];
-  //Conditional Function
-  //const COnditionaFunc = () => {
-  //Conditional Statement here
-  //return Cities.length === 1 ? alert("on point bro") : null;
-  //Conditional Statement here
-  // };
-  const HandleClick = (index) => console.log(index);
+
+  // Declare a piece of state to keep track of the selected item.
+  const [SelectedItem, setSelectedItem] = useState(-1);
+
   return (
+    // Use a React fragment to group multiple elements without introducing an additional DOM element.
     <>
+      {/* Display a heading. */}
       <h2>A bunch of List Items</h2>
 
+      {/* Create an unordered list with a class name. */}
       <ul className="list-group">
-        {Cities.map((city) => (
-          <li className="list-group-item" onClick={HandleClick} key={city}>
+        {/* Map over the 'Cities' array and render a list item for each city. */}
+        {Cities.map((city, index) => (
+          <li
+            // Set the class name for the list item. If the 'index' matches the 'SelectedItem', it gets the 'active' class.
+            className={
+              SelectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            // Add an 'onClick' event handler to select the clicked item.
+            onClick={() => {
+              setSelectedItem(index);
+            }}
+            // Set a 'key' attribute to help React identify list items when they change or re-order.
+            key={city}
+          >
             {city}
           </li>
         ))}
@@ -23,4 +39,6 @@ function ListGroup() {
     </>
   );
 }
+
+// Export the 'ListGroup' component for use in other parts of your application.
 export default ListGroup;
