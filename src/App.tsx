141e6c4516ee5e9ec786a2f4ceb0061d2ca08620
TORY-SKY <ezevictor559@gmail.com>
2023-11-05 18:03:08 +0100

    react

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ function App() {
   //const Cities = ["Lagos", "Texas", "Fineland", "Mexico", "Bahamas", "Madrid"];
   return (
     <>
-      <Button children="Click me please" color="btn btn-sucess" />
+      <Button children="Click me please" />
+
+      <div>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-3xl ">Hello, world!</h1>
+          <p className="text-gray-500">
+            This is a React application with Tailwind CSS.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
